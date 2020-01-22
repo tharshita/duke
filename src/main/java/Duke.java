5d4b891afae565b3440b76620cc70ff4e0a77bb4
tharshita <>
2020-01-22 12:15:03 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm \n" + logo + "\nWhat can I do for you?");
+        while(sc.hasNext()) {
+            String input = sc.next();
+            if(input.toLowerCase().contains("bye")) {
+                System.out.println("  " + "Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println("  " + input);
+        }
     }
 }
