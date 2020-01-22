81c07f57e0d5e19ea0f0c0ae2617750b6e7699f6
tharshita <>
2020-01-23 02:12:15 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //initialize item array
+        ArrayList<String> items = new ArrayList<String>();
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,13 +15,24 @@ public class Duke {
         System.out.println("Hello I'm \n" + logo + "\nWhat can I do for you?");
         while(sc.hasNext()) {
             String input = sc.next();
+
             if(input.toLowerCase().contains("bye")) {
                 //exit when "bye"
                 System.out.println("  " + "Bye. Hope to see you again soon!");
                 break;
 
+            } else if(input.equalsIgnoreCase("list")) {
+                for(int i=0; i < items.size(); i++) {
+                    //print out everything one by one
+                    System.out.println(i+1 + ". " + items.get(i));
+                }
+
+            } else{
+                //add item if not goodbye
+                items.add(input);
+                System.out.println("  added: " + input);
             }
-            System.out.println("  " + input);
+
         }
     }
 }
