ff1c1777716ab6a96caa6821925fcf023bd7b008
tharshita <>
2020-01-23 03:14:40 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,16 +5,17 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //initialize item array
-        ArrayList<String> items = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello I'm \n" + logo + "\nWhat can I do for you?");
-        while(sc.hasNext()) {
-            String input = sc.next();
+        while(sc.hasNextLine()) {
+            String input = sc.nextLine();
 
             if(input.toLowerCase().contains("bye")) {
                 //exit when "bye"
@@ -22,14 +23,24 @@ public class Duke {
                 break;
 
             } else if(input.equalsIgnoreCase("list")) {
-                for(int i=0; i < items.size(); i++) {
+                //loop through tasks array to list
+                for (int i = 0; i < tasks.size(); i++) {
                     //print out everything one by one
-                    System.out.println(i+1 + ". " + items.get(i));
+                    Task curr = tasks.get(i);
+                    System.out.println(i + 1 + ". [" + curr.getStatusIcon() + "] " + curr.getDescription());
                 }
+            }else if(input.contains("done")) {
+                //task number and change isDone to true
+                String[] arr = input.split(" ");
+                int eventNum = Integer.parseInt(arr[1]);
+                Task curr = tasks.get(eventNum-1);
+                curr.markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("  " + "[" + curr.getStatusIcon() + "] " + curr.getDescription());
 
-            } else{
+            }else{
                 //add item if not goodbye
-                items.add(input);
+                tasks.add(new Task(input));
                 System.out.println("  added: " + input);
             }
 
