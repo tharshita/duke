72f43d9d1cc431323727b2135c0a2747b4000662
tharshita <>
2020-02-01 13:22:11 +0800

    javaFx tut 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * Creates an instance of storage, ui, tasklist and parser in the main.
  * Starts running the program.
  */
 
-public class Duke {
+public class Duke extends Application {
 
     private Storage storage;
     private TaskList tasks;
@@ -33,6 +36,19 @@ public class Duke {
     public void run() {
         ui.sayHi();
         parser.run();
+    }
+
+    /**
+     * Overrides the Application#start() method and provide a concrete implementation.
+     * @param stage primary stage that JavaFX provides.
+     */
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     /**
